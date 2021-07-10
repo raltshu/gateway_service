35660e25e5ada69971be964269d4af83ada57541
raltshu <raltshu@gmail.com>
2021-07-10 15:53:57 +0300

    changes

--- src/handlers/home_proxy.py
+++ src/handlers/home_proxy.py
@@ -1,15 +1,19 @@
+import os
+import requests
 from flask_classful import FlaskView, route
 from flask import request,render_template
+
+dataservice = os.environ['dataservice_endpoint']
 
 class HomeView(FlaskView):
 
     def index(self):
         #TODO: Data service get first 20 lines for display
+        response = requests.get(url=f"{dataservice}/data")
         print('Hello')
-        return render_template('home/list.html')
+        return render_template('home/list.html', data=response.text)
     
     # @route('/predict', methods=['GET'])
     # def predict_view(self):
     #     return render_template('predict.html')
-
 
